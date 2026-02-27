dd122c5c308a67f7b3f05482f68d3bf7a080fef0
indrol12 <indrolofficial@gmail.com>
2026-02-27 09:54:23 +0530

    demoblaze feature file and glue code added

---
+++ automation_testing23jan/src/main/java/stepdefinations/demoblaze.java
@@ -0,0 +1,53 @@
+package stepdefinations;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class demoblaze {
+	ChromeDriver driver;
+@Given("launch demoblaze website")
+	public void launch_demoblaze()
+	{
+		driver=new ChromeDriver();
+		driver.get("https://www.demoblaze.com/");
+	    driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+	}
+@When("user click on sign up button on navbar")
+ public void signup()
+ {
+	driver.findElement(By.id("signin2")).click();
+ }
+@When("user enter username")
+	public void username()
+	{
+		driver.findElement(By.id("sign-username")).sendKeys("yogita");
+	}
+@And("user enter password")
+	public void password()
+	{
+		driver.findElement(By.id("sign-password")).sendKeys("yogita123");
+	}
+@And("click on sign up button")
+	public void signupbutton()
+	{
+		driver.findElement(By.xpath("//button[@onclick='register()']")).click();
+	}
+@Then("demoblaze sign up successfull and should display alert message")
+	public void display_alert() throws InterruptedException
+	{
+	    Thread.sleep(2000);
+		Alert al=driver.switchTo().alert();
+		System.out.println("Alert message:"+al.getText());
+	    al.accept();
+		driver.close();
+	}
+}
